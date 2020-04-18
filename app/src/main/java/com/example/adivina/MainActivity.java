ed669b2d1711f42fda1b5a4d8afe2841bfd555ec
LeandroEmanuel <leandrog276@gmail.com>
2020-04-18 01:27:47 +0100

    alteracaod  de cores

--- app/src/main/java/com/example/adivina/MainActivity.java
+++ app/src/main/java/com/example/adivina/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.adivina;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.ViewParent;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Random random = new Random();
     public int numeroGerado;
-    public int tentativas;
+    public int tentativas = 0;
     public boolean acertou = false;
 
     @Override
@@ -27,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
        //  Toast.makeText(this,"numero\n"+ numeroGerado,Toast.LENGTH_SHORT).show();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void tentativa(View view) {
 
         EditText editTextValor = (EditText) findViewById(R.id.valorAdivinha);
@@ -52,16 +58,24 @@ public class MainActivity extends AppCompatActivity {
             button.setText(R.string.eviarButton);
             numeroGerado = random.nextInt(10)+1;
             tentativas = 0;
-            resultado.setText(R.string.eviarButton+"\t"+tentativas+getString(R.string.tentativas));
+            resultado.setText(getString(R.string.resultado)+"\t"+tentativas+getString(R.string.tentativas));
             acertou = false;
+
         } else{
             tentativas++;
             if(numeroDigitado == numeroGerado){// compara o numero digitado com o geradpo
                 resultado.setText(getString(R.string.acertou_depois_de)+tentativas+getString(R.string.tentativas));
                 button.setText(R.string.recomeçar);
                 acertou = true;
+
+                /*if (Build.VERSION.SDK_INT >= 21) {
+                    getWindow().setNavigationBarColor(Color.GREEN); // fundo
+                    getWindow().setStatusBarColor(Color.CYAN); // topo
+                    getWindow().setNavigationBarDividerColor(Color.GRAY);
+                }*/
+
             } else{
-                resultado.setText(R.string.eviarButton+"\t"+tentativas+getString(R.string.tentativas));
+                resultado.setText(getString(R.string.resultado)+"\t"+tentativas+getString(R.string.tentativas));
             }
         }
     }
